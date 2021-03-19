7f6edad656c4a9f68d065662fa363f757d48e8a9
llcruz1 <lucaslimacruz1@gmail.com>
2021-03-18 21:05:07 -0300

    Adding Router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="/">Proshop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand href="/">Proshop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="/cart">
+              <LinkContainer to="/cart">
                 <i className="fas fa-shopping-cart"></i>Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
+              </LinkContainer>
+              <LinkContainer to="/login">
                 <i className="fas fa-user"></i>Login
-              </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
